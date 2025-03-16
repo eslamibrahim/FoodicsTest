6081cf5e72fc12b05da4f80bbac4608a95c62b32
Islam Awaad <iawwad-e@jahez.net>
2025-03-16 19:58:38 +0300

    Initial Commit

---
+++ foodics Task/foodics_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodics_TaskApp.swift
+//  foodics Task
+//
+//  Created by islam Awaad on 16/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct foodics_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
